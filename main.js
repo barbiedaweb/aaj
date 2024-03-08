1d7ac98c664fd7f80ad6ea4affcf7d879b2f375e
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-03-08 12:18:30 -0300

    .

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ while(nome_Usuario == '' ){ // || nome_Usuario == null
     nome_Usuario = prompt('Qual é o seu nome?');
 }
 
-if(nome_Usuario == null){
-texto.textContent = 'usuário';
+if(nome_Usuario == null){ //se o usuario for null ele irá ficar vazio 
+texto.textContent = 'usuário'; 
 }else{
 texto.textContent = nome_Usuario; 
 }
