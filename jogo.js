8b0889eed9601d7ebba63fc9bb71ca557ff5c8c8
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-03-11 12:07:12 -0300

    .

--- jogo.js
+++ jogo.js
@@ -6,9 +6,9 @@ while (chute == '' || chute == null){
     chute = prompt('Escolha um número 0 a 10'); // se o chute for vazio ou nulo ele irá continuar perguntando
 }
 
-if(numeroSecreto == chute){
-    alert('Parabens, você acertou!!!'); 
+if(numeroSecreto == chute){ 
+    alert('Parabens, você acertou!!!'); // se o chute for certo irá aparecer 'Parabens, você acertou!!!'
 } else{
-    alert('Que triste, você errou :(');
+    alert('Que triste, você errou :('); // se o chute for errado irá aparecer 'Que triste, você errou :('
 }
 
