7bc811e4c562e407394774b4c5f1bbb257aca923
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-03-11 11:55:22 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,9 +1,13 @@
 let numeroSecreto = 9;
 
-let chute = prompt('Escolha um número 0 a 10'); 
+let chute = '';
+
+while (chute == '' || chute == null){
+    chute = prompt('Escolha um número 0 a 10'); // se o chute for vazio ou nulo ele irá continuar perguntando
+}
 
 if(numeroSecreto == chute){
-    alert('Parabens, você acertou!!!');
+    alert('Parabens, você acertou!!!'); 
 } else{
     alert('Que triste, você errou :(');
 }
