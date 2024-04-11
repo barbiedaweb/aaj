e45b080a0b56f7d6677a35b54e3f19383abb4eb1
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-04-11 09:02:48 -0300

    ;;;

--- cont.js
+++ cont.js
@@ -5,8 +5,10 @@ console.log(botoes.length);
 for(let i = 0; i<botoes.length; i++) { //se 
 console.log(i);
 botoes[i].onclick = function(){
-botoes[i].classList.add('ativo');
-//botoes[0].class.List.remove('ativo');
+for (let j = 0; j < botoes.length; j++) {
+botoes[j].class.List.remove('ativo');
 }
+    botoes[i].classList.add('ativo');
+    }
 }
 
