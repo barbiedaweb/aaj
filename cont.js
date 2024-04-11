8a79f228c47a7c9738b4b67c20c314683992c116
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-04-11 09:07:13 -0300

    jj

--- cont.js
+++ cont.js
@@ -6,7 +6,7 @@ for(let i = 0; i<botoes.length; i++) { //se
 console.log(i);
 botoes[i].onclick = function(){
 for (let j = 0; j < botoes.length; j++) {
-botoes[j].class.List.remove('ativo');
+botoes[j].classList.remove('ativo');
 }
     botoes[i].classList.add('ativo');
     }
