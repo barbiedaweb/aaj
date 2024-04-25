3cdf86f0780b7d4152afe73c4cf765a11415ca4f
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-04-25 09:24:35 -0300

    .......

--- cont.js
+++ cont.js
@@ -24,7 +24,12 @@ let tempoObjetivo4 = new Date ('2024-12-13T00:00');
 
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+for(let i = 0< contadores.length; i++){
+    contadores[i].textContent = CalculaTempo(tempos[i]);
+}
+
+function CalculaTempo(tempoObjetivo) {
+let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
@@ -34,4 +39,6 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
+}
