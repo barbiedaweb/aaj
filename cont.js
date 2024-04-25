68c379aa2a81707ebc2b45ddff19ce5163b82181
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-04-25 09:08:39 -0300

    //////

--- cont.js
+++ cont.js
@@ -17,11 +17,15 @@ for (let j = 0; j < botoes.length; j++) {
 
 const contadores= document.querySelectorAll('.contador');
 let tempoAtual = new Date ();
-let tempoObjetivo1 = new Date ('2024-04-19T00:00');
+let tempoObjetivo1 = new Date ('2024-04-30T00:00');
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
+
+segundos %= 60;
+minutoa %= 60;
+horas %= 24;
 
 contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
